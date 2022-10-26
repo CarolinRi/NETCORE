91af7a5cf22f83b956d26d644514d030271f3828
Carolin Rickert <Carolin.rickert@tum.de>
2022-10-26 13:29:26 +0200

    Add files via upload

---
+++ test_NETCORE.py
@@ -0,0 +1,34 @@
+"""
+Test the NETCORE algorithm with example datasets
+Version: 1.0
+Python 3.9+
+Date created: October 26th, 2022
+Date modified: -
+
+
+HOW TO RUN THIS ALGORITHM
+    - Load the test datasets from https://github.com/CarolinRi/NETCORE
+    - Unzip the file and place the folder into the same directory in which the test_NETCORE.py file is located (alternatively, you can change the list containing the filenames to include the respective directories)
+    - Make sure you have the NETCORE algorithm installed (easiest way: load NETCORE.py from https://github.com/CarolinRi/NETCORE) and have it in the same directory as the test_NETCORE.py file
+    - if neccessary, install the required packages (mainly networkx and pandas) or directly use the anaconda environment accessible at https://github.com/CarolinRi/NETCORE. (easiest way is to import it via the Anaconda Navigator: https://anaconda.org/anaconda/anaconda-navigator)
+    - run test_NETCORE.py
+"""
+
+from NETCORE import *
+import pandas as pd
+
+
+# specify the file with the data to be analyzed
+filenames = ["Test_datasets/Antibiotics.csv","Test_datasets/Fluorophores.csv","Test_datasets/Vitamins.csv","Test_datasets/Antioxidants.csv","Test_datasets/Pooled.csv"]
+
+# specify the correlation threshold to be used for creating the correlation matrix
+t_corr = 0.6
+
+for name in filenames:
+    print(f"analyzed dataset: {name}")
+
+    # import data as a pandas data frame
+    data = pd.read_csv(name)
+
+    # run the NETCORE algorithm
+    network_based_elimination(data, t_corr)
